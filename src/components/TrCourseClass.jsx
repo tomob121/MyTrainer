175c5dbdcd9c9cce2ae41d1405fa19faef1933c3
Tomislav <tbaric1@gmail.com>
2022-07-01 12:12:02 +0200

    Cleand up

--- src/components/TrCourseClass.jsx
+++ src/components/TrCourseClass.jsx
@@ -236,7 +236,6 @@ class TrCourseClass extends Component {
                   }
                   handleDelete={(e) => this.handleDelete(trainingLine)}
                 />
-                {/* {this.RenderRepChangeAndDelete(isEditing, style, trainingLine)} */}
                 {isEditing ? (
                   ''
                 ) : (
@@ -265,65 +264,6 @@ class TrCourseClass extends Component {
           />
         </div>
       </Container>
-    );
-  }
-
-  RenderRepChangeAndDelete(isEditing, style, trainingLine) {
-    return (
-      <div className='col-auto row'>
-        {isEditing ? (
-          <div className='row'>
-            <div className='col-auto'>
-              <input
-                onFocus={this.handleFocus}
-                min={0}
-                type={'number'}
-                max={999}
-                style={style.repNumberStyle}
-                key={trainingLine.id}
-                onChange={(e) =>
-                  this.handleLineChange(e, trainingLine.id, 'reps')
-                }
-                value={trainingLine.reps}
-              />
-              <Button
-                onClick={(e) => this.handleDelete(trainingLine)}
-                style={{ marginLeft: 30 }}
-                className='btn-danger'
-              >
-                Delete
-              </Button>
-            </div>
-            <div className='col-2'>
-              <input
-                onFocus={this.handleFocus}
-                maxLength='80'
-                style={style.repNumberStyle}
-                value={trainingLine.note}
-                placeholder={'Enter note here'}
-                onChange={(e) =>
-                  this.handleLineChange(e, trainingLine.id, 'note')
-                }
-              />
-            </div>
-          </div>
-        ) : (
-          <div className='col-auto row'>
-            <div className='col-auto' style={style.repNumberStyle}>
-              {trainingLine.reps}
-            </div>
-          </div>
-        )}
-        {isEditing ? (
-          ''
-        ) : (
-          <div className='col-auto pt-1' style={style.toolTipMessage}>
-            {trainingLine.exercise.type.toLowerCase() === 'endure'
-              ? 'Seconds'
-              : 'Reps'}
-          </div>
-        )}
-      </div>
     );
   }
 }
